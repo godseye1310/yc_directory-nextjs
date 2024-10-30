1ecbb75b56cb1f4304937b4c6dfb221ee9dd443e
Avinash <avinashkmg1310@gmail.com>
2024-10-30 08:40:32 +0530

    Updating views count server-side SSR (PPR). +Sanity write-client

--- components/View.tsx
+++ components/View.tsx
@@ -1,8 +1,9 @@
 import Ping from "@/components/Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
 import { Eye } from "lucide-react";
-import React from "react";
+import { unstable_after as after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
 	const { views: totalViews } = await client
@@ -12,6 +13,13 @@ const View = async ({ id }: { id: string }) => {
 		});
 
 	// TODO: Update the number of view count
+	after(
+		async () =>
+			await writeClient
+				.patch(id)
+				.set({ views: totalViews + 1 })
+				.commit()
+	);
 
 	return (
 		<div className="view-container ">
